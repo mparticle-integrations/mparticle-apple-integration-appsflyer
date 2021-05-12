048f2787f76c44beabcc48a78bf9c6a6bb14051c
Peter Jenkins <pjenkins@mparticle.com>
2021-05-12 10:35:57 -0400

    fix: Update import syntax for SPM (#35)

--- Sources/mParticle-AppsFlyer/include/mParticle_AppsFlyer.h
+++ Sources/mParticle-AppsFlyer/include/mParticle_AppsFlyer.h
@@ -8,4 +8,10 @@ FOUNDATION_EXPORT const unsigned char mParticle_AppsFlyerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <mParticle_AppsFlyer/PublicHeader.h>
 
+#if defined(__has_include) && __has_include(<mParticle_AppsFlyer/MPKitAppsFlyer.h>)
 #import <mParticle_AppsFlyer/MPKitAppsFlyer.h>
+#else
+#import "MPKitAppsFlyer.h"
+#endif
+
+
