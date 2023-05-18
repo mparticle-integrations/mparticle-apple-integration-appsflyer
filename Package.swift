a393583f9629d5e6629c9d9a906330c2ae8652e8
Johann Rodríguez <johann@johannrodriguez.com>
2023-05-18 16:49:39 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
                .upToNextMajor(from: "8.0.0")),
       .package(name: "AppsFlyerLib",
                url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework",
-               .upToNextMajor(from: "6.8.0")),
+               .upToNextMajor(from: "6.11.0")),
     ],
     targets: [
         .target(
