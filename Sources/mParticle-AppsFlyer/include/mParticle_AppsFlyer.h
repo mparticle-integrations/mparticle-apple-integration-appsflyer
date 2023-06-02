505a28abb6a952668eff752e2d5ccf77157f4c99
Ben Baron <bbaron@mparticle.com>
2023-06-02 14:19:49 -0700

    feat: Add NoLocation target to SPM (#42)

--- Sources/mParticle-AppsFlyer/include/mParticle_AppsFlyer.h
+++ Sources/mParticle-AppsFlyer/include/mParticle_AppsFlyer.h
@@ -9,9 +9,11 @@ FOUNDATION_EXPORT const unsigned char mParticle_AppsFlyerVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <mParticle_AppsFlyer/PublicHeader.h>
 
 #if defined(__has_include) && __has_include(<mParticle_AppsFlyer/MPKitAppsFlyer.h>)
-#import <mParticle_AppsFlyer/MPKitAppsFlyer.h>
+    #import <mParticle_AppsFlyer/MPKitAppsFlyer.h>
+#elif defined(__has_include) && __has_include(<mParticle_AppsFlyer_NoLocation/MPKitAppsFlyer.h>)
+    #import <mParticle_AppsFlyer/MPKitAppsFlyer.h>
 #else
-#import "MPKitAppsFlyer.h"
+    #import "MPKitAppsFlyer.h"
 #endif
 
 
