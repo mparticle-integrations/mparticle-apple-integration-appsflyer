3c39c5678ce33c220ac3404d9c0d791fe11c95e0
Brandon Stalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2023-07-26 12:43:39 -0400

    Apply suggestions from code review
    Co-authored-by: Ben Baron <bbaron@mparticle.com>

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -5,11 +5,6 @@ NOTES="$2"
 # Update version number
 #
 
-# Update Carthage release json file
-jq --indent 3 '. += {'"\"$VERSION\""': "'"https://github.com/mparticle-integrations/mparticle-apple-integration-appsflyer/releases/download/$PREFIXED_VERSION/mParticle_AppsFlyer.framework.zip?alt=https://github.com/mparticle-integrations/mparticle-apple-integration-appsflyer/releases/download/$PREFIXED_VERSION/mParticle_AppsFlyer.xcframework.zip"'"}'
-mParticle_AppsFlyer.json > tmp.json
-mv tmp.json mParticle_AppsFlyer.json
-
 # Update CocoaPods podspec file
 sed -i '' 's/\(^    s.version[^=]*= \).*/\1"'"$VERSION"'"/' mParticle-AppsFlyer.podspec
 
--- release.config.js
+++ release.config.js
@@ -30,9 +30,6 @@ module.exports = {
         "@semantic-release/github",
         {
           assets: [
-            "mParticle_AppsFlyer.framework.zip",
-            "mParticle_AppsFlyer.xcframework.zip",
-            "generated-docs.zip",
           ],
         },
       ],
